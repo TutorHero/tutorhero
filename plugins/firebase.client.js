9f26e6e98e7efd068ba50dca8a429a8b4b289732
zyangchuan <zyangchuan1@gmail.com>
2025-03-31 11:41:07 +0800

    install firebase

---
+++ plugins/firebase.client.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+
+export default defineNuxtConfig(() => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAUwtAu6H6TmgIQimkcIIDzLoz7q5cJI-w",
+    authDomain: "tutorhero-fc1bf.firebaseapp.com",
+    projectId: "tutorhero-fc1bf",
+    storageBucket: "tutorhero-fc1bf.firebasestorage.app",
+    messagingSenderId: "616430032594",
+    appId: "1:616430032594:web:820d5cdb1c2cc91bc1ff0b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  return {
+    provide: {
+      firebaseApp: app
+    }
+  }
+})

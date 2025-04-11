8d7434a32ac80566cd5ad28e88e80802e8f4146c
j4ryl <165132550+j4ryl@users.noreply.github.com>
2025-04-11 19:51:39 +0800

    Create studentStore.js

---
+++ stores/studentStore.js
@@ -0,0 +1,35 @@
+import { defineStore } from 'pinia'
+import { addStudent, listAllStudents } from '@firebasegen/default-connector';
+
+
+// Call the `listAllTutors()` function to execute the query.
+// You can use the `await` keyword to wait for the promise to resolve.
+
+
+export const useStudentStore = defineStore('studentStore', {
+    state: () => {
+        return {
+            students: [],
+        }
+    },
+    actions: {
+        async fetchAllStudents() {
+            try {
+                const { data } = await listAllStudents();
+                console.log(data);
+                this.students = data;
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        async createStudent(tutor) {
+            try {
+                const { data } = await addStudent(tutor);
+                console.log(data);
+                await this.fetchAllStudents();
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    }
+})

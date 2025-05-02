3d3c9d14386dd4dfc961b0e7f913c63b30a1820a
j4ryl <165132550+j4ryl@users.noreply.github.com>
2025-05-02 10:16:33 +0800

    added subjects handling with tutor

--- frontend/pages/test.vue
+++ frontend/pages/test.vue
@@ -5,10 +5,20 @@
 <script setup>
 import { useLinkStore } from '../stores/linkStore';
 const { $firebaseAuth, $firebaseDataConnect } = useNuxtApp();
-import { getTutorStudents } from  '@firebasegen/default-connector'
+import { getTutorStudents, createSubject, getSubjectByName, createTutorSubject } from '@firebasegen/default-connector'
 const linkStore = useLinkStore();
 onMounted(async () => {
   const date = new Date();
+  const { data } = await getSubjectByName({ name: "english" });
+  let id = data.subjects[0]?.id;
+
+  if (!id) {
+    const { data: createData } = await createSubject({ name: "english" });
+    id = createData?.id; // or whatever shape your response has
+  }
+
+  const { data: data1 } = await createTutorSubject({ subjectId: id });
+  console.log(data1);
   await linkStore.createLink(date);
   const link = linkStore.link;
   console.log(link);

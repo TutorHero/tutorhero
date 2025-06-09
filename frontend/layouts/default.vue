784c7b3afe70475fb2ded763134bd8ce68663567
zyangchuan <zyangchuan1@gmail.com>
2025-06-09 15:32:58 +0800

    Remove duplicate students tab

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -25,13 +25,6 @@
               </NavigationMenuLink>
             </NuxtLink>
           </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NuxtLink v-slot="{ isActive, href, navigate }" to="/students" custom>
-              <NavigationMenuLink :active="isActive" :href="href" :class="navigationMenuTriggerStyle()" @click="navigate">
-                Students
-              </NavigationMenuLink>
-            </NuxtLink>
-          </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
     </div>
